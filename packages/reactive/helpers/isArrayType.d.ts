39c127e4afa912c0e9713a13c869c51f2d24ffbf
Rateria <shubham@custiv.com>
2018-08-03 03:06:58 +0530

    chore: cleanup

--- packages/reactive/helpers/isArrayType.d.ts
+++
@@ -1,2 +0,0 @@
-declare const _default: (val: any) => boolean;
-export default _default;
--- packages/reactive/sync/index.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * We can do two things,
- * event emit when a new element is added to the output buffer and that gets sent.
- * check output buffer every second, and if any elements are present then output them
- */
-export declare function Sync(obj: any, socketEndpoint: string | undefined, socketConfig: {
-    path: string;
-} | undefined, onChange: any): any;
