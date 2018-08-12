e7c266ab4167ccafb2174a0c2d0b35c402ed527f
Rateria <shubham@custiv.com>
2018-08-12 23:52:29 +0530

    feat: fixes and refactoring for send and receiving patches after delete

--- packages/reactive/src/observables/object/patch/patch.ts
+++ packages/reactive/src/observables/object/patch/patch.ts
@@ -26,23 +26,33 @@ function getTokens(path: string) {
 }
 
 export function evaluate(object: any, path: string): PointerEvaluation {
+  console.log("[evaluate:start]", object, path);
   const tokens = getTokens(path);
 
   let parent: any = null;
   let key = "";
   let value = object || undefined;
   for (let i = 1, l = tokens.length; i < l; i++) {
+    console.log("[evaluate:token]", tokens[i]);
     parent = value;
 
     key = tokens[i];
-    if (key == "__proto__" || key == "constructor" || key == "prototype") {
-      continue;
-    }
+    // if (key == "__proto__" || key == "constructor" || key == "prototype") {
+    //   continue;
+    // }
     // not sure if this the best way to handle non-existant paths...
     if (parent) {
+      console.log(
+        "[eval:object:tokens:before:query]",
+        key,
+        typeof parent,
+        value
+      );
       value = parent[key];
+      console.log("[eval:object:tokens:after:query]", key, parent, value);
     }
     // value = (parent || {})[key];
+    console.log("[eval:object:tokens]", tokens[i], parent, value);
   }
 
   return { parent, key, value };
@@ -94,6 +104,7 @@ export function apply(
   //   {add, remove, replace, move, copy, test}[operation.op](object, operation)
   // (seems like a bug)
 
+  console.log("[obejct:patch:eval:incoming]", object, operation);
   const { parent, value, key } = evaluate(object, operation.path);
   const patch: TPatch = operation;
   patch.path = `/${key}`;
