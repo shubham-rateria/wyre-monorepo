d7fe2e6f8786b11f3fbb9db7f2f5ec59bb66ba7a
Rateria <shubham@custiv.com>
2018-12-03 22:13:52 +0530

    chore: testing streamer

---
+++ apps/consumer/app.js
@@ -0,0 +1,20 @@
+const { createWyre } = require("@wyre-client/core");
+
+const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
+
+async function startStreamer() {
+  const sync = createWyre({
+    data: { todos: [] },
+    onChange: () => {
+      console.log("received data");
+    },
+  });
+  const data = await sync.init("streamer:testing:2");
+  // for (let i = 0; i < 1000; i++) {
+  //   data.todos.push(i);
+  //   console.log("[streaming]", i);
+  //   await sleep(2000);
+  // }
+}
+
+startStreamer();
