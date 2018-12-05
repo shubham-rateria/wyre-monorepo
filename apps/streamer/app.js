fc7c89115d7f41fc123d26abb22363dd24ab7f03
Rateria <shubham@custiv.com>
2018-12-06 04:25:37 +0530

    chore: testing

--- apps/streamer/app.js
+++ apps/streamer/app.js
@@ -7,11 +7,16 @@ async function startStreamer() {
   const sync = createWyre({
     data: { todos: [] },
   });
-  const data = await sync.init("streamer:testing:2");
+  const data = await sync.init("syncKey1");
+  const data2 = await sync.init("syncKey2");
+  const data3 = await sync.init("syncKey3");
   for (let i = 0; i < 20; i++) {
     data.todos.push(i);
     console.log("[streaming]", i);
-    await sleep(2000);
+    await sleep(1000);
+    if (i > 5) {
+      await sleep(10000);
+    }
   }
 }
 
