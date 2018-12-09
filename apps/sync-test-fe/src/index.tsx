7c29b17ce3bdc1b5433df7895725d5d79459c53c
Rateria <shubham@custiv.com>
2018-12-09 13:30:10 +0530

    chore: testing fixes

--- apps/sync-test-fe/src/index.tsx
+++ apps/sync-test-fe/src/index.tsx
@@ -15,7 +15,7 @@ import { StreamerTest } from "./StreamerTest/StreamerTest";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<Todo />);
+root.render(<TestGCL />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
