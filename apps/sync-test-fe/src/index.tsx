007633524202f5706ef43ae0aabfa58563796346
Rateria <shubham@custiv.com>
2023-02-20 19:06:13 +0530

    feat: massive refactor

--- apps/sync-test-fe/src/index.tsx
+++ apps/sync-test-fe/src/index.tsx
@@ -11,11 +11,7 @@ import { Dict } from "./Dict/Dict";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <Todo />
-  </React.StrictMode>
-);
+root.render(<Todo />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
