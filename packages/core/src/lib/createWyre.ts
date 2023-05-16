897ef26d7b2d416fc94dc1b3ac63b3c433f17a4c
Rateria <shubham@custiv.com>
2023-05-16 14:16:20 +0530

    fix: no hook usage

---
+++ packages/core/src/lib/createWyre.ts
@@ -0,0 +1,43 @@
+import { SyncManager } from "../sync/sync";
+
+interface UseSyncParams {
+  data: any;
+  collectionName?: string;
+  onChange?: () => void;
+}
+
+export const createWyre = (params: UseSyncParams) => {
+  const onChange = () => {
+    console.log("[onChange]");
+    if (params.onChange) {
+      params.onChange();
+    }
+  };
+
+  //   const sync = async () => {
+  //     const syncData = await SyncManager.sync(id);
+  //     if (syncData) {
+  //       // @ts-ignore
+  //       loadedData.setRawValues(syncData);
+  //       setValue((value) => value + 1);
+  //     }
+  //   };
+
+  /**
+   *
+   * @param id the unique identifier for the data
+   * @returns a javascript object that is real time and shared
+   */
+  const init: (id: string) => Promise<any> = async (id: string) => {
+    console.log("[createWyre:init]");
+    const loadedData: any = await SyncManager.create({
+      data: params.data,
+      collectionName: params.collectionName ?? "",
+      refid: id ?? "",
+      onChange,
+    });
+    return loadedData;
+  };
+
+  return { init };
+};
